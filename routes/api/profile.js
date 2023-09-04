5fd57f6f0808237661f70f06b552bf8bfa148f71
hatim85f <hatim.fayez85@gmail.com>
2023-09-05 02:57:01 +0400

    profile

---
+++ routes/api/profile.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const auth = require("../../middleware/auth");
+const User = require("../../models/User");
+const { default: mongoose } = require("mongoose");
+const router = express.Router();
+
+// @route   GET api/profile
+// @desc    Test route
+// @access  Public
+
+// get user profile
+// userName, phone, email, designation, phoneVerified, mailVerified, from users collection
+// number of businesses, and details of businesses from businesses collection
+// number of team members, from businessUsers collection
+router.get("/:userId", auth, async (req, res) => {
+  const { userId } = req.params;
+
+  try {
+    const userProfile = await User.aggregate([
+      {
+        $match: {
+          _id: new mongoose.Types.ObjectId(userId),
+        },
+      },
+      {
+        $lookup: {
+          from: "businessusers",
+          localField: "_id",
+          foreignField: "userId",
+          as: "user_business",
+        },
+      },
+      {
+        $lookup: {
+          from: "businesses",
+          localField: "user_business.businessId",
+          foreignField: "_id",
+          as: "business",
+        },
+      },
+
+      {
+        $project: {
+          _id: 1,
+          userName: 1,
+          phone: 1,
+          email: 1,
+          designation: 1,
+          phoneVerified: 1,
+          mailVerified: 1,
+          business: 1,
+          numberOfBusinesses: { $size: "$business" },
+        },
+      },
+    ]);
+
+    return res.status(200).send(userProfile);
+  } catch (error) {
+    return res.status(500).send({ error: "Error", message: error.message });
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/business", require("./routes/api/business"));
 app.use("/api/products", require("./routes/api/products"));
 app.use("/api/team", require("./routes/api/team"));
