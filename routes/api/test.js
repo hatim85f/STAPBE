4f853cf2a7e574f2a7e2e72308de01beb796e6d4
hatim85f <hatim.fayez85@gmail.com>
2023-10-12 01:55:04 +0400

    mobile

--- routes/api/test.js
+++ routes/api/test.js
@@ -50,4 +50,81 @@ router.post("/", auth, async (req, res) => {
   }
 });
 
+router.post("/create-payment-intent", async (req, res) => {
+  const { amount, currency, paymentMethodType, userId, packageId } = req.body;
+
+  try {
+    // Find the user based on userId and exclude the password field
+    const user = await User.findOne({ _id: userId }).select("-password");
+
+    // Retrieve the customer's Stripe information using their email
+    const stripeCustomer = await stripe.customers.list({ email: user.email });
+
+    // check if current customer, if not create new customer
+    if (stripeCustomer.data.length === 0) {
+      const customer = await stripe.customers.create({
+        email: user.email,
+        name: user.userName,
+      });
+      stripeCustomer.data.push(customer);
+    }
+
+    // Check if the customer has any subscriptions
+    if (stripeCustomer.data.length === 0) {
+      return res.status(200).send({ packagesSubscribed: [] });
+    }
+
+    // Retrieve the customer's subscriptions
+    const subscriptions = await stripe.subscriptions.list({
+      customer: stripeCustomer.data[0].id,
+    });
+
+    // Check if there is a subscription for the specified packageId
+    for (const subscription of subscriptions.data) {
+      const package = await Package.findOne({
+        stripeProductId: subscription.items.data[0].price.product,
+      });
+      if (package && package._id.toString() === packageId) {
+        // Subscription for the same package found
+        const endDate = subscription.current_period_end;
+        return res.status(200).send({
+          message: `You already have a subscription for the selected package, which will end on ${new Date(
+            endDate * 1000
+          ).toDateString()}`,
+        });
+      }
+    }
+
+    // create ephemeral key for the customer for app
+    const ephemeralKey = await stripe.ephemeralKeys.create(
+      { customer: stripeCustomer.data[0].id },
+      { apiVersion: "2020-08-27" }
+    );
+
+    const setupIntent = await stripe.setupIntents.create({
+      customer: stripeCustomer.data[0].id,
+    });
+
+    // If no subscription for the specified packageId is found, create the paymentIntent
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: amount * 100,
+      currency: currency,
+      payment_method_types: ["card"],
+      setup_future_usage: "off_session",
+      customer: stripeCustomer.data[0].id,
+    });
+
+    return res.status(200).send({
+      clientSecret: paymentIntent.client_secret,
+      ephemeralKey: ephemeralKey.secret,
+      customer: stripeCustomer.data[0].id,
+      paymentIntentId: paymentIntent.id,
+      setupIntent: setupIntent.client_secret,
+    });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).send({ error: "Error", message: error.message });
+  }
+});
+
 module.exports = router;
