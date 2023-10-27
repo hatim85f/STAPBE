908105e5fa3e327a042087ee9738bd042d0c3374
hatim85f <hatim.fayez85@gmail.com>
2023-10-28 02:55:55 +0400

    editing customer

--- middleware/isCompanyAdmin.js
+++ middleware/isCompanyAdmin.js
@@ -4,19 +4,18 @@ const User = require("../models/User");
 // Create a middleware function to check if a user is authorized (SuperAdmin or Organizer)
 module.exports = async (req, res, next) => {
   const userId = req.header("userId");
-  const businessId = req.header("businessId");
 
   const user = await User.findOne({ _id: userId });
   const userType = user.userType;
 
-  if (userType !== "Business Owner" || userType !== "Business Admin") {
+  if (userType === "Business Owner" || userType === "Business Admin") {
+    // If the user is authorized, continue to the next middleware/route handler
+    next();
+  } else {
     return res.status(403).json({
       error: "Unauthorized",
       message:
         "You are not authorized to perform this action. please contact your admin.",
     });
   }
-
-  // if admin or business owner continue
-  next();
 };
