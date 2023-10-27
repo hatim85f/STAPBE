65c77318cddd509fdda2c8e6514c32bc5c612344
hatim85f <hatim.fayez85@gmail.com>
2023-10-28 02:46:47 +0400

    client with adding business logo

--- middleware/isCompanyAdmin.js
+++ middleware/isCompanyAdmin.js
@@ -9,21 +9,6 @@ module.exports = async (req, res, next) => {
   const user = await User.findOne({ _id: userId });
   const userType = user.userType;
 
-  // check if the user belongs to the business
-
-  const isBusinessUser = await BusinessUsers.findOne({
-    userId,
-    businessId,
-  });
-
-  if (!isBusinessUser) {
-    return res.status(403).json({
-      error: "Unauthorized",
-      message:
-        "You are not authorized to perform this action. please contact your admin.",
-    });
-  }
-
   if (userType !== "Business Owner" || userType !== "Business Admin") {
     return res.status(403).json({
       error: "Unauthorized",
