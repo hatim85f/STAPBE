271ac394d6e6ec152397d3fe7ece8ee06f90fd60
hatim85f <50639901+hatim85f@users.noreply.github.com>
2023-11-01 22:59:41 +0400

    phasing

--- routes/api/phasing.js
+++ routes/api/phasing.js
@@ -3,9 +3,28 @@ const router = express.Router();
 const Phasing = require("../../models/Phasing");
 const auth = require("../../middleware/auth");
 const isCompanyAdmin = require("../../middleware/isCompanyAdmin");
+const BusinessUsers = require("../../models/BusinessUsers");
 
-router.get("/", auth, async (req, res) => {
-  return res.status(200).json({ msg: "Phasing route works" });
+router.get("/:userId", auth, async (req, res) => {
+  const userId = req.params.userId;
+
+  try {
+    const business = await BusinessUsers.find({ userId });
+
+    const businessIds = business.map((business) => business.businessId);
+
+    const phasing = await Phasing.find({ businessId: { $in: businessIds } });
+
+    if (!phasing) {
+      return res
+        .status(400)
+        .json({ error: "Error", message: "No phasing data found" });
+    }
+
+    return res.status(200).json({ phasing });
+  } catch (error) {
+    return res.status(500).json({ error: "Error", message: "Server error" });
+  }
 });
 
 router.post("/", auth, isCompanyAdmin, async (req, res) => {
