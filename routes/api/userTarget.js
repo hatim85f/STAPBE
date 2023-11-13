a1bd90ca7bdf8132a2aa5ecf01f100c896dac062
hatim85f <50639901+hatim85f@users.noreply.github.com>
2023-11-13 21:37:14 +0400

    editing cost price

--- routes/api/userTarget.js
+++ routes/api/userTarget.js
@@ -139,7 +139,7 @@ router.post("/", auth, isCompanyAdmin, async (req, res) => {
   } catch (error) {
     return res.status(500).send({
       error: "Error",
-      message: "Something went wrong, please try again later",
+      message: error.message,
     });
   }
 });
