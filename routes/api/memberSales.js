7180e04e9fa83e35680e1bce727e0c08514c3025
hatim85f <50639901+hatim85f@users.noreply.github.com>
2024-01-15 20:59:44 +0400

    issue

--- routes/api/memberSales.js
+++ routes/api/memberSales.js
@@ -20,6 +20,8 @@ router.get("/:userId/:month/:year", auth, async (req, res) => {
     const startDate = new Date(year, month - 1, 1);
     const endDate = new Date(year, month, 0);
 
+    console.log(startDate);
+
     const salesVersions = await UserSales.aggregate([
       {
         $match: {
