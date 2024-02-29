836638711e9686c43f937ed321fba7cd8e5964a3
hatim85f <50639901+hatim85f@users.noreply.github.com>
2024-02-29 21:23:21 +0400

    modifying user tokens

--- routes/api/pushToken.js
+++ routes/api/pushToken.js
@@ -8,32 +8,44 @@ router.post("/", auth, async (req, res) => {
   const { userId, token } = req.body;
 
   try {
-    // check if the user has a push token reigestered
+    // check if the user has a push token registered
     const userToken = await PushToken.findOne({ user: userId });
 
     if (userToken) {
-      await PushToken.updateMany(
-        { user: userId },
-        {
-          $addToSet: {
-            token: token,
-          },
-        }
-      );
+      if (userToken.token === token) {
+        // Token already exists, no need to update
+        return res.status(200).end(); // Send an empty response
+      } else {
+        // Add the new token to the user's list of tokens
+        await PushToken.updateMany(
+          { user: userId },
+          {
+            $addToSet: {
+              token: token,
+            },
+          }
+        );
+        return res.status(200).send({
+          message:
+            "Your new device has been registered to receive push notifications. If you don't want to receive notifications, please disable it in the settings.",
+        });
+      }
     } else {
+      // Create a new PushToken document for the user
       const newPushToken = await new PushToken({
         user: userId,
         token: token,
       });
 
-      await PushToken.insertMany(newPushToken);
+      // Insert the new PushToken document
+      await PushToken.insertMany([newPushToken]);
+      return res.status(200).send({
+        message:
+          "Your device has been registered to receive push notifications. If you don't want to receive notifications, please disable it in the settings.",
+      });
     }
-
-    return res.status(200).send({
-      message:
-        "Your device has been registered to receive push notifications, if you don't want to receive notifications, please disable it in the settings.",
-    });
   } catch (error) {
+    console.error(error);
     return res.status(500).json({ message: "Internal Server Error" });
   }
 });
