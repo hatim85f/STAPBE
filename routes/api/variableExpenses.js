1d4ff5bcd3f4e9d45fe62bf04c6d7e8ce0412ae2
hatim85f <50639901+hatim85f@users.noreply.github.com>
2024-03-04 23:19:01 +0400

    deleting variable expenses

--- routes/api/variableExpenses.js
+++ routes/api/variableExpenses.js
@@ -311,7 +311,7 @@ router.put("/update/:id", auth, async (req, res) => {
   }
 });
 
-router.delete("/delete/:id", auth, async (req, res) => {
+router.delete("/:id", auth, async (req, res) => {
   const { id } = req.params;
 
   try {
