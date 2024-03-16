aad3f44ed9a026a3a0d378efe600dd7e47dc357d
hatim85f <50639901+hatim85f@users.noreply.github.com>
2024-03-17 00:23:01 +0400

    adding partner

---
+++ models/Partner.js
@@ -0,0 +1,81 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const PartnerSchema = Schema({
+  business: {
+    type: mongoose.Types.ObjectId,
+    ref: "business",
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  profileImage: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  DOB: {
+    type: Date,
+    required: true,
+  },
+  idDetails: [
+    {
+      idType: {
+        type: String,
+        required: true,
+        enum: ["Passport", "National ID"],
+      },
+      idImage: {
+        type: String,
+        required: true,
+      },
+      idNumber: {
+        type: String,
+        required: true,
+      },
+      idExpire: {
+        type: Date,
+        required: true,
+      },
+    },
+  ],
+  bankDetails: [
+    {
+      bankName: {
+        type: String,
+        required: true,
+      },
+
+      bankIBAN: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  percentage: {
+    type: Number,
+    required: true,
+  },
+  dateOfStart: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+  responsibilities: {
+    type: Array,
+  },
+});
+
+module.exports = Partner = mongoose.model("partner", PartnerSchema);
---
+++ routes/api/partner.js
@@ -0,0 +1,228 @@
+const express = require("express");
+const router = express.Router();
+const Partner = require("../../models/Partner");
+const Business = require("../../models/Business");
+const auth = require("../../middleware/auth");
+const User = require("../../models/User");
+const bcrypt = require("bcryptjs");
+const isCompanyAdmin = require("../../middleware/isCompanyAdmin");
+const BusinessUsers = require("../../models/BusinessUsers");
+
+// @route   GET api/partner
+// @desc    Get all partners
+// @access  Private
+router.get("/:businessId", auth, isCompanyAdmin, async (req, res) => {
+  const { businessId } = req.params;
+
+  try {
+    const partners = await Partner.find({ business: businessId });
+    return res.status(200).json(partners);
+  } catch (error) {
+    return res.status(500).json({ message: "Server Error, Please try again" });
+  }
+});
+
+// @route   POST api/partner
+// @desc    Create a new partner
+// @access  Private
+router.post("/", auth, async (req, res) => {
+  const {
+    business,
+    name,
+    email,
+    phone,
+    profileImage,
+    address,
+    idType,
+    idImage,
+    idNumber,
+    idExpire,
+    bankName,
+    bankIBAN,
+    percentage,
+    dateOfStart,
+    responsiblitites,
+    DOB,
+    password,
+  } = req.body;
+
+  try {
+    // hasing the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // // create a normal user with a role of partner
+    const newUser = User({
+      userName: name,
+      email,
+      phone,
+      profilePicture: profileImage,
+      DOB,
+      joiningDate: dateOfStart,
+      userType: "Partner",
+      firstName: name.split(" ")[0],
+      lastName: name.split(" ")[1],
+      password: hashedPassword,
+      biometricEnabled: false,
+      designation: "Partner",
+      emailVerified: false,
+      phoneVerified: false,
+      isAuthorized: false,
+      authority: ["Partner"],
+      isActivated: true,
+    });
+
+    await newUser.save();
+
+    // // change idExpire string to date
+    const day = idExpire.split("/")[0];
+    const month = idExpire.split("/")[1];
+    const year = idExpire.split("/")[2];
+
+    const idExpireDate = new Date(year, month - 1, day);
+
+    const newPartner = new Partner({
+      business,
+      name,
+      email,
+      phone,
+      profileImage,
+      address,
+      idDetails: [
+        {
+          idType,
+          idImage,
+          idNumber,
+          idExpire: idExpireDate,
+        },
+      ],
+      bankDetails: [
+        {
+          bankName,
+          bankIBAN,
+        },
+      ],
+      percentage,
+      dateOfStart,
+      responsiblitites,
+      DOB,
+    });
+
+    await Partner.insertMany(newPartner);
+
+    const newBusinessUser = new BusinessUsers({
+      userId: newUser._id,
+      businessId: business,
+      isBusinessOwner: false,
+    });
+
+    await BusinessUsers.insertMany(newBusinessUser);
+
+    return res.status(200).json({
+      message: `Partner ${name} created successfully, and a user account is created successfully`,
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Server Error, Please try again later" });
+  }
+});
+
+router.put("/:id", auth, async (req, res) => {
+  const { id } = req.params;
+  const {
+    name,
+    email,
+    phone,
+    profileImage,
+    address,
+    idType,
+    idImage,
+    idNumber,
+    idExpire,
+    bankName,
+    bankIBAN,
+    percentage,
+    dateOfStart,
+    responsiblitites,
+    DOB,
+  } = req.body;
+
+  try {
+    const partner = await Partner.findOne({ _id: id });
+
+    if (!partner) {
+      return res.status(404).json({ message: "Partner not found" });
+    }
+
+    //change idExpire string to date if the type is string
+    if (typeof idExpire === "string") {
+      const day = idExpire.split("/")[0];
+      const month = idExpire.split("/")[1];
+      const year = idExpire.split("/")[2];
+
+      idExpire = new Date(year, month - 1, day);
+    }
+
+    await Partner.updateMany(
+      {
+        _id: id,
+      },
+      {
+        $set: {
+          name,
+          email,
+          phone,
+          profileImage,
+          address,
+          idDetails: [
+            {
+              idType,
+              idImage,
+              idNumber,
+              idExpire,
+            },
+          ],
+          bankDetails: [
+            {
+              bankName,
+              bankIBAN,
+            },
+          ],
+          percentage,
+          dateOfStart,
+          responsiblitites,
+          DOB,
+        },
+      }
+    );
+
+    return res.status(200).json({ message: "Partner updated successfully" });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Server Error, Please try again later" });
+  }
+});
+
+router.delete("/:id", auth, async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const partner = await Partner.findOne({ _id: id });
+
+    if (!partner) {
+      return res.status(404).json({ message: "Partner not found" });
+    }
+
+    await Partner.deleteOne({ _id: id });
+
+    return res.status(200).json({ message: "Partner deleted successfully" });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Server Error, Please try again later" });
+  }
+});
+
+module.exports = router;
